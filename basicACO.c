0f3f1c00a918292c382d029dace7c88b421962e0
Lalo <tim.shanksu@gmail.com>
2013-02-27 12:49:17 -0600

    just configuring business (calculation of the  path)

--- basicACO.c
+++ basicACO.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
+#include <sys/time.h>
+
+
 #define VERBOSE
 
 typedef struct {
@@ -11,10 +14,11 @@ typedef struct {
 } graph_t;
 
 graph_t  get_distance_matrix();
-void initialize_feromone(float * incomplete_graph);
-float calc_prob(int ant, float * array_of_prob, int elements, int * eval_array, int * graph);
+void initialize_feromone(float * incomplete_graph, float * originalGraph, int number_of_ants);
+void calc_path(int * ant_city_travel, float * graph, int number_of_ants, float * feromone_matrix, int alpha, int beta);
 float calculateDistanceBetween(int coord_a_x , int coord_a_y, int coord_b_x, int coord_b_y);
 void initialize_ant_distribution(int * ant_matrix, int number_of_ants);
+void choose_city(int currentCity,int * ant_city_travel, float * path, int * visitedCities, float * feromone_path, int alpha, int beta);
 
 int main(int argc, char const *argv[])
 {
@@ -35,6 +39,12 @@ int main(int argc, char const *argv[])
     }
     #endif
 
+    int alpha, beta;
+    printf("alpha: \n");
+    scanf("%d",&alpha);
+    printf("beta: \n");
+    scanf("%d",&beta);
+
     int * ant_matrix = (int *)malloc( sizeof(int) * (instance.width * instance.height));
     int * matrix_pointer = ant_matrix;
      for (int i = 0; i < instance.width; ++i)
@@ -47,41 +57,68 @@ int main(int argc, char const *argv[])
     }
     initialize_ant_distribution(ant_matrix, instance.width);
 
+    int counter = 0;
+
     for (int i = 0; i < instance.width; ++i)
     {
-        printf("%d\n",ant_matrix[i]);
+        for (int j = 0; j < instance.height; ++j, counter++)
+        {
+            printf("%d ",ant_matrix[counter]);
+        }
+        printf("\n");
     }
 
+   
+
+
+    int * ant_travel_current_path = ant_matrix;
+    int number_of_ants = instance.width;
+    float * the_distance_matrix = instance.matrix;
+    float * feromone_matrix = (float *)malloc(sizeof(float) * (number_of_ants * number_of_ants));
+    float * pointerToFeromoneMatrix = feromone_matrix;
+
+    initialize_feromone(pointerToFeromoneMatrix, the_distance_matrix, number_of_ants);
+
+     //testing probability function
+    // TODO : implement probability function within a cycle
+
+    calc_path(ant_travel_current_path, the_distance_matrix, number_of_ants, feromone_matrix, alpha, beta);
+
+    free(feromone_matrix);
     free(instance.matrix);
     return 0;
 }
+
+
 
 void initialize_ant_distribution(int * ant_matrix, int number_of_ants)
 {
     int random_city;
     int cont = 0;
-    while(cont < number_of_ants){
-        int * begining = ant_matrix;
+    int * arrayOfConcurrency = (int *) malloc(sizeof(int)* number_of_ants);
 
-        for (int i = 0; i < number_of_ants ; i++)
-        {
-            srand(time(0));
-            random_city = (rand() % number_of_ants) + 1;
+    for (int i = 0; i < number_of_ants; ++i)
+    {
+        arrayOfConcurrency[i] = 0;
+    }
+    int * begining = ant_matrix;
 
-            if (*begining == random_city){
-
+    while(cont < number_of_ants -1){
+        while(1){
+            struct timeval t;
+            gettimeofday(&t,NULL);
+            srand(t.tv_sec+t.tv_usec);
+            random_city = rand() % number_of_ants;
+            if (arrayOfConcurrency[random_city]==0)
+            {
+                printf("value = %d\n",random_city);
+                arrayOfConcurrency[random_city]=1;
                 break;
-
-            } else if (*begining == 0) {
-                printf("random_city = %d\n", random_city);
-
-                *begining = random_city;
-                cont ++;
-                break;
-
             }
-            begining++;
         }
+
+        *(begining + (cont * number_of_ants)) = random_city;
+        cont++;
     }
     
 }
@@ -158,4 +195,62 @@ float calculateDistanceBetween(int coord_a_x , int coord_a_y, int coord_b_x, int
     result = sqrt(result);
 
     return result;
+}
+
+
+void calc_path(int * ant_city_travel, float * graph, int number_of_ants, float * feromone_matrix, int alpha, int beta){
+
+    int * visitedCities = (int *)malloc(sizeof(int) * number_of_ants);
+    int currentCity;
+    float * eval_path;
+    float * feromone_path;
+    for (int i = 0; i < number_of_ants; ++i)
+    {
+        visitedCities[i] = 0;
+    }
+
+    for (int i = 0; i < number_of_ants -1; ++i)
+    {
+        currentCity = ant_city_travel[i];
+        visitedCities[currentCity] = 1;
+        eval_path = &graph[currentCity * number_of_ants];
+        feromone_path = &feromone_matrix[currentCity * number_of_ants];
+
+        choose_city(currentCity, ant_city_travel, eval_path, visitedCities, feromone_path, alpha, beta);
+    }
+
+
+
+    free(visitedCities);
+
+}
+
+
+// here is the bussiness
+
+void choose_city(int currentCity,int * ant_city_travel, float * path, int * visitedCities, float * feromone_path, int alpha, int beta){
+
+
+
+}
+
+
+void initialize_feromone(float * incomplete_graph, float * originalGraph, int number_of_ants){
+    for (int i = 0; i < number_of_ants; ++i)
+    {
+        for (int j = 0; j < number_of_ants; ++j,originalGraph++, incomplete_graph++)
+        {
+            if (*originalGraph != 0)
+            {
+                *incomplete_graph = 0.001;
+                
+            }
+            else
+            {
+                *incomplete_graph = 0;
+            }
+            printf("%.3f ", *incomplete_graph );
+        }
+        printf("\n");
+    }
 }
